486fdf66ba8f6745d55fb72f7ad6e3929ab61f8e
Tero Keski-Valkama <tero.keski-valkama@cybercom.com>
2014-12-17 17:39:03 +0200

    Added NFC reading events.

--- client/www/js/index.js
+++ client/www/js/index.js
@@ -1,6 +1,7 @@
 var app = {
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        nfc.addNdefListener(onReadNdefTag);
     },
     onDeviceReady: function() {
         var parentElement = document.getElementById('app');
@@ -9,5 +10,11 @@ var app = {
 
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+    },
+    onreadNdefTag: function(nfcEvent) {
+        var parentElement = document.getElementById('app');
+        var outputElement = parentElement.querySelector('.output_element');
+
+        outputElement.innerHtml = "Read NFC tag with contents: " + JSON.stringify(nfcEvent);
     }
 };
