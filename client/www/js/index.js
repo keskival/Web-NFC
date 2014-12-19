3ea9fc3c128a45c8a8b3e0e1e95b37d1a8018b35
Tero Keski-Valkama <tero.keski-valkama@cybercom.com>
2014-12-19 22:39:44 +0200

    Replaced the example files with the first PoC implementation. Reading NFC chips now works locally.

--- client/www/js/index.js
+++ client/www/js/index.js
@@ -1,49 +1,37 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+var debugElement = document.getElementById('debug');
+var waitingElement = document.getElementById('waiting');
+var okElement = document.getElementById('ok');
+var outputElement = document.getElementById('output');
+var readNFCIndicatorElement = document.getElementById('readNFC');
+
 var app = {
-    // Application Constructor
     initialize: function() {
-        this.bindEvents();
+        debugElement.innerHTML = debugElement.innerHTML + 'Initializing...\n';
+        document.addEventListener('deviceready', app.onDeviceReady, false);
+        debugElement.innerHTML = debugElement.innerHTML + 'Initialized.\n';
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+        debugElement.innerHTML = debugElement.innerHTML + 'onDeviceReady.\n';
+        waitingElement.setAttribute('style', 'display: none;');
+        okElement.setAttribute('style', 'display: block;');
+
+        nfc.addNdefListener(app.onReadNdefTag, app.win, app.fail);
+
+        debugElement.innerHTML = debugElement.innerHTML + 'onDeviceReady done.\n';
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    onReadNdefTag: function(nfcEvent) {
+        var tag = nfcEvent.tag,
+            ndefMessage = tag.ndefMessage;
+        debugElement.innerHTML = debugElement.innerHTML + 'onReadNdefTag...\n';
+        outputElement.innerHTML = "Read NFC tag with contents: " + JSON.stringify(ndefMessage) + ", payload: " +
+            nfc.bytesToString(ndefMessage[0].payload).substring(3);
+        readNFCIndicatorElement.setAttribute('style', 'display: block;');
+        debugElement.innerHTML = debugElement.innerHTML + 'NDEF tag read.\n';
+    },
+    win: function() {
+        debugElement.innerHTML = debugElement.innerHTML + 'win!';
+    },
+    fail: function(reason) {
+        debugElement.innerHTML = debugElement.innerHTML + 'fail: ' + JSON.stringify(reason);
     }
 };
